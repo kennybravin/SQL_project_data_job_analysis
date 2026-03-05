50da99cdd1660d178b4197f7f7bd637a279aa411
Kennedy Liaka <kenbravin12@gmail.com>
2026-03-05 12:20:59 +0300

    insight analysis added

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -20,3 +20,17 @@ FROM top_paying_jobs
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY salary_year_avg DESC;
+
+/*
+This query identifies the top-paying remote jobs for Data Analysts and retrieves the associated skills for those jobs. It uses Common Table Expressions (CTEs) to first select the top-paying jobs and then joins with the skills tables to get the relevant skills for those jobs.
+
+Insight Summary:
+
+    SQL is the most demanded skill in top-paying roles.
+
+    Python is nearly as important and appears in most of the jobs.
+
+    Visualization tools like Tableau are highly valued.
+
+    Excel still matters, even in high-salary analytics jobs.
+*/
